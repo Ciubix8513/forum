eddab42ab24633368187d5aa3b709377f0e728ab
Luna <ciubix8514@gmail.com>
2022-12-11 11:44:42 +0300

    Added post component

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using forum;
 using Microsoft.AspNetCore.Components.Authorization;
 using Bwasm.Cookies.Provider;
 using Bwasm.Cookies.Handler;
 using Blazored.LocalStorage;
 using Blazored.Modal;
 using Bwasm.Cookies.Logic;
+using forum;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddBlazorStrap();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
